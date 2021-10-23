32013b715769623028081ec3e915a894b1451168
Ibe Stephen <saintkalson@gmail.com>
2021-10-23 12:07:59 +0100

    check for rows  of 3 and 4

--- src/App.js
+++ src/App.js
@@ -32,6 +32,68 @@ function App() {
     }
   };
 
+  const checkForThreeRow = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currColorArrangement[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+      if (
+        rowOfThree.every(
+          (square) => currColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfThree.forEach((square) => (currColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  const checkForFourRow = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const decidedColor = currColorArrangement[i];
+      const notValid = [
+        5,
+        ,
+        6,
+        7,
+        13,
+        14,
+        15,
+        21,
+        22,
+        23,
+        29,
+        30,
+        31,
+        37,
+        38,
+        39,
+        45,
+        46,
+        47,
+        53,
+        54,
+        55,
+        62,
+        63,
+        64,
+      ];
+
+      if (notValid.includes(i)) continue;
+      if (
+        rowOfFour.every(
+          (square) => currColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfFour.forEach((square) => (currColorArrangement[square] = ""));
+      }
+    }
+  };
+
   const checkForFourColumn = () => {
     for (let i = 0; i < 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
@@ -54,11 +116,18 @@ function App() {
   useEffect(() => {
     const timer = setInterval(() => {
       checkForFourColumn();
+      checkForFourRow();
       checkForThreeColumn();
+      checkForThreeRow();
       setCurrColorArrangement([...currColorArrangement]);
     }, 100);
     return () => clearInterval(timer);
-  }, [checkForFourColumn, checkForThreeColumn, currColorArrangement]);
+  }, [
+    checkForFourColumn,
+    checkForThreeColumn,
+    checkForThreeRow,
+    currColorArrangement,
+  ]);
 
   console.log(currColorArrangement);
 
