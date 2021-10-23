502e5e4035ba41f33b9fb0bc8285d57110655495
Ibe Stephen <saintkalson@gmail.com>
2021-10-23 08:51:55 +0100

    generate random colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currColorArrangement, setCurrColorArrangement] = useState([]);
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+
+    setCurrColorArrangement(randomColorArrangement);
+  };
+
+  console.log(currColorArrangement);
+
+  return <div></div>;
 }
 
 export default App;
