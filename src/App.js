77b4f76ce6301fa8430cd2efff0bfc84805ce5f7
Ibe Stephen <saintkalson@gmail.com>
2021-10-23 09:42:20 +0100

    check for columns of 3

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 function App() {
   const [currColorArrangement, setCurrColorArrangement] = useState([]);
 
-  useEffect(() => {
-    createBoard();
-  }, []);
-
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -21,9 +17,49 @@ function App() {
     setCurrColorArrangement(randomColorArrangement);
   };
 
+  const checkForThreeColumn = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach((square) => (currColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForThreeColumn();
+      setCurrColorArrangement([...currColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkForThreeColumn, currColorArrangement]);
+
   console.log(currColorArrangement);
 
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="game__board">
+        {currColorArrangement.length !== 0 &&
+          currColorArrangement.map((candyColor, i) => (
+            <img
+              key={i}
+              style={{ backgroundColor: candyColor }}
+              // alt={candyColor}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
