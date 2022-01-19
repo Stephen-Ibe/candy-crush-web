c54d5a95d7c8baeffef55d35724700244fd63f50
KalsonSaint <saintkalson@gmail.com>
2022-01-19 17:24:56 +0100

    Fetch Scores in FE

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { React, useEffect, useState } from "react";
+
 const Scoreboard = ({ score }) => {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8000/scores");
+    setData(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="score-board">
       <h2>{score}</h2>
